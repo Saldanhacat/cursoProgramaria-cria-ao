2aad08d29c006704344e8e196d3bda9bf28dcd45
Cátia Saldanha <65631190+Saldanhacat@users.noreply.github.com>
2020-10-13 17:16:00 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("botaoEnviar").addEventListener("click",validaformulario)
+
+function validaformulario(){
+  if(document.getElementById("nome").value != "" && document.getElementById("e-mail").value != "" && document.getElementById("telefone").value != "" ){
+    alert("Prontinho! Você receberá as novidades por e-mail.")
+  }else{
+    alert("Por favor, preencha os campos nome, e-mail e telefone.")}
+  }
